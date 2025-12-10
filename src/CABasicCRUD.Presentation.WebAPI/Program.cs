9b0b5e616c288c3ee5a639e52507303ae125bb89
Admin <108695191+mgsbb@users.noreply.github.com>
2025-12-11 01:20:08 +0530

    feat(api): add main entrypoint to web api
    Register application and persistence services.
    Add controllers and swagger.

---
+++ src/CABasicCRUD.Presentation.WebAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using CABasicCRUD.Application;
+using CABasicCRUD.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterApplicationServices();
+builder.Services.RegisterPersistenceServices(configuration: builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
