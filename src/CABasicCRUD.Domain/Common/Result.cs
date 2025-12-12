9d50077aae0f7376a3d2e222d18e8b420291e9a9
Admin <108695191+mgsbb@users.noreply.github.com>
2025-12-13 01:43:55 +0530

    feat(domain): add Result without generics

--- src/CABasicCRUD.Domain/Common/Result.cs
+++ src/CABasicCRUD.Domain/Common/Result.cs
@@ -1,21 +1,36 @@
 namespace CABasicCRUD.Domain.Common;
 
-public class Result<TValue>
+public class Result
 {
-    public bool IsSuccess { get; private set; }
+    public bool IsSuccess { get; protected set; }
     public bool IsFailure => !IsSuccess;
-    public Error? Error { get; private set; }
-    public TValue? Value { get; private set; }
+    public Error? Error { get; protected set; }
 
-    private Result(bool isSuccess, TValue? value, Error? error)
+    protected Result(bool isSuccess, Error? error)
     {
         if (isSuccess && error != null)
             throw new InvalidOperationException();
 
-        if (isSuccess && value == null)
+        if (!isSuccess && error == null)
             throw new InvalidOperationException();
 
-        if (!isSuccess && error == null)
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Success() => new(true, null);
+
+    public static Result Failure(Error? error) => new(false, error);
+}
+
+public class Result<TValue> : Result
+{
+    public TValue? Value { get; private set; }
+
+    private Result(bool isSuccess, TValue? value, Error? error)
+        : base(isSuccess, error)
+    {
+        if (isSuccess && value == null)
             throw new InvalidOperationException();
 
         IsSuccess = isSuccess;
@@ -25,7 +40,7 @@ public class Result<TValue>
 
     public static Result<TValue> Success(TValue? value) => new(true, value, null);
 
-    public static Result<TValue> Failure(Error? error) => new(false, default, error);
+    public static new Result<TValue> Failure(Error? error) => new(false, default, error);
 
     public static implicit operator Result<TValue>(TValue? value) => Success(value);
 }
