7b0fb1e2239f4cd597676b14891fac444ecfaa9d
Admin <108695191+mgsbb@users.noreply.github.com>
2025-12-27 12:02:44 +0530

    refactor(app)!: make ICurrentUser non-nullable
    BREAKING CHANGE: UserId and Email in ICurrentUser are no longer nullable.
    Ensure user is authenticated before accessing these properties.

--- src/CABasicCRUD.Application/Common/Interfaces/ICurrentUser.cs
+++ src/CABasicCRUD.Application/Common/Interfaces/ICurrentUser.cs
@@ -2,7 +2,7 @@ namespace CABasicCRUD.Application.Common.Interfaces;
 
 public interface ICurrentUser
 {
-    Guid? UserId { get; }
-    string? Email { get; }
+    Guid UserId { get; }
+    string Email { get; }
     bool IsAuthenticated { get; }
 }
--- src/CABasicCRUD.Presentation.WebAPI/Common/Security/CurrentUser.cs
+++ src/CABasicCRUD.Presentation.WebAPI/Common/Security/CurrentUser.cs
@@ -10,18 +10,20 @@ internal sealed class CurrentUser(IHttpContextAccessor httpContextAccessor) : IC
 
     private ClaimsPrincipal? User => _httpContextAccessor.HttpContext?.User;
 
-    public Guid? UserId
+    public Guid UserId
     {
         get
         {
             string? guidString = User?.FindFirstValue(JwtRegisteredClaimNames.Sub);
             if (guidString is null)
-                return null;
+                throw new UnauthorizedAccessException();
             return Guid.Parse(guidString);
         }
     }
 
-    public string? Email => User?.FindFirstValue(JwtRegisteredClaimNames.Email);
+    public string Email =>
+        User?.FindFirstValue(JwtRegisteredClaimNames.Email)
+        ?? throw new UnauthorizedAccessException();
 
     public bool IsAuthenticated => User?.Identity?.IsAuthenticated == true;
 }
