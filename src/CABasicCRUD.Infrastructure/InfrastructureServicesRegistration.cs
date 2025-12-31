a0b66c12a0eddb0aa5b8d15e39a5587ce0cdf44f
Admin <108695191+mgsbb@users.noreply.github.com>
2025-12-31 18:14:25 +0530

    refactor(infra): add jwt configuration to infrastructure

--- src/CABasicCRUD.Infrastructure/InfrastructureServicesRegistration.cs
+++ src/CABasicCRUD.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using CABasicCRUD.Application.Common.Interfaces;
 using CABasicCRUD.Domain.Services;
 using CABasicCRUD.Infrastructure.Authentication;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CABasicCRUD.Infrastructure;
@@ -8,9 +9,13 @@ namespace CABasicCRUD.Infrastructure;
 public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection RegisterAuthenticationServices(
-        this IServiceCollection services
+        this IServiceCollection services,
+        IConfiguration configuration
     )
     {
+        services.Configure<JwtOptions>(configuration.GetSection("Jwt"));
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
         services.AddScoped<IJwtProvider, JwtProvider>();
         services.AddScoped<IPasswordHasher, PasswordHasher>();
 
