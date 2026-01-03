1dc536c5f3da64557c60ea9893a5634b2b408c0a
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-03 13:41:42 +0530

    feat(host): add host for pgsql and webapi
    Secrets configured in user secrets.

---
+++ src/CABasicCRUD.Host.PostgreSql.WebApi/Program.cs
@@ -0,0 +1,28 @@
+﻿using CABasicCRUD.Application;
+using CABasicCRUD.Infrastructure;
+using CABasicCRUD.Infrastructure.Persistence.PostgreSql;
+using CABasicCRUD.Presentation.WebApi;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.RegisterApplicationServices();
+builder.Services.RegisterAuthenticationServices(builder.Configuration);
+builder.Services.RegisterPersistenceServices(builder.Configuration);
+builder.Services.RegisterPresentationServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
