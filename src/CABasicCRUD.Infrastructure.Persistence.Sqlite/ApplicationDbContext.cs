a350284cfbaf8b6229dda3606290413127567303
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-05 01:23:49 +0530

    refactor(infra): separate IUnitOfWork from ApplicationDbContext
    Project: Infrastructure.Persistence.Sqlite

--- src/CABasicCRUD.Infrastructure.Persistence.Sqlite/ApplicationDbContext.cs
+++ src/CABasicCRUD.Infrastructure.Persistence.Sqlite/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using CABasicCRUD.Application.Common.Interfaces;
 using CABasicCRUD.Domain.Comments;
 using CABasicCRUD.Domain.Posts;
 using CABasicCRUD.Domain.Users;
@@ -8,19 +7,13 @@ using Microsoft.EntityFrameworkCore;
 namespace CABasicCRUD.Infrastructure.Persistence.Sqlite;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions)
-    : DbContext(dbContextOptions),
-        IUnitOfWork
+    : DbContext(dbContextOptions)
 {
     public DbSet<Post> Posts { get; set; }
 
     public DbSet<User> Users { get; set; }
 
     public DbSet<Comment> Comments { get; set; }
-
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        return base.SaveChangesAsync(cancellationToken);
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
