5ce05e36ce0bf44d7ef454132159a29d424f5e29
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-08 22:12:35 +0530

    fix(app): remove deliberate exception
    Used for simulating retry policy.

--- src/CABasicCRUD.Application/Features/Auth/UserRegisteredDomainEventHandler.cs
+++ src/CABasicCRUD.Application/Features/Auth/UserRegisteredDomainEventHandler.cs
@@ -18,7 +18,6 @@ public sealed class UserRegisteredDomainEventHandler
         CancellationToken cancellationToken
     )
     {
-        throw new Exception("Deliberate exception");
         await _emailSender.SendWelcomeEmailAsync(
             domainEvent.Name,
             domainEvent.Email,
