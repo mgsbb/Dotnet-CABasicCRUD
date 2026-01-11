35c1f23b8dae9381327fc6c4c2a405f641683fc3
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-11 19:22:38 +0530

    feat(host): add serilog, exception middleware and rate limiter
    Project: Host.PostgreSql.WebApi

--- src/CABasicCRUD.Host.PostgreSql.WebApi/Program.cs
+++ src/CABasicCRUD.Host.PostgreSql.WebApi/Program.cs
@@ -2,13 +2,21 @@
 using CABasicCRUD.Infrastructure;
 using CABasicCRUD.Infrastructure.Persistence.PostgreSql;
 using CABasicCRUD.Presentation.WebApi;
+using CABasicCRUD.Presentation.WebApi.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.RegisterApplicationServices();
-builder.Services.RegisterAuthenticationServices(builder.Configuration);
+
+builder.Services.RegisterInfrastructureServices(builder.Configuration);
 builder.Services.RegisterPersistenceServices(builder.Configuration);
+
 builder.Services.RegisterPresentationServices();
+
+builder.Host.UseSerilog(
+    (context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)
+);
 
 var app = builder.Build();
 
@@ -18,8 +26,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+app.UseSerilogRequestLogging();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapControllers();
 
