95609ee1b5e22151e546481d6d358cdcdce1b29a
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-11 18:52:58 +0530

    feat(migrations): add audit fields migrations
    Project: Infrastructure.Persistence.PostgreSql

---
+++ src/CABasicCRUD.Infrastructure.Persistence.PostgreSql/Migrations/20260111131845_AddAuditFields.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CABasicCRUD.Infrastructure.Persistence.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: true
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Comments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
+            );
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Comments",
+                type: "timestamp with time zone",
+                nullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "CreatedAt", table: "Users");
+
+            migrationBuilder.DropColumn(name: "UpdatedAt", table: "Users");
+
+            migrationBuilder.DropColumn(name: "CreatedAt", table: "Posts");
+
+            migrationBuilder.DropColumn(name: "UpdatedAt", table: "Posts");
+
+            migrationBuilder.DropColumn(name: "CreatedAt", table: "Comments");
+
+            migrationBuilder.DropColumn(name: "UpdatedAt", table: "Comments");
+        }
+    }
+}
