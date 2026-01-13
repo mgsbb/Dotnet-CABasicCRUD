c8ce83f8cd1cfad1860db25e7100187404204d4a
Admin <108695191+mgsbb@users.noreply.github.com>
2026-01-13 10:07:11 +0530

    feat(host): add open telemetry
    Project: Host.PostgreSql.WebApi

--- src/CABasicCRUD.Host.PostgreSql.WebApi/Program.cs
+++ src/CABasicCRUD.Host.PostgreSql.WebApi/Program.cs
@@ -3,6 +3,10 @@ using CABasicCRUD.Infrastructure;
 using CABasicCRUD.Infrastructure.Persistence.PostgreSql;
 using CABasicCRUD.Presentation.WebApi;
 using CABasicCRUD.Presentation.WebApi.Middlewares;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder();
@@ -17,6 +21,24 @@ builder.Services.RegisterPresentationServices();
 builder.Host.UseSerilog(
     (context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)
 );
+
+builder
+    .Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation().AddHttpClientInstrumentation().AddOtlpExporter();
+    })
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddOtlpExporter();
+    });
+
+builder.Logging.AddOpenTelemetry(logging => logging.AddOtlpExporter());
 
 var app = builder.Build();
 
