77fb59fe7070131b378273260b79ed5d42d8cee4
Admin <108695191+mgsbb@users.noreply.github.com>
2026-02-22 14:49:08 +0530

    feat(infra): add ConversationType to Conversation table
    Project: Infrastructure.Persistence.PostgreSql
    ConversationType enum has string conversion. Add migration files.

---
+++ src/CABasicCRUD.Infrastructure.Persistence.PostgreSql/Migrations/20260222091347_AddConversationType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CABasicCRUD.Infrastructure.Persistence.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConversationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConversationType",
+                table: "Conversations",
+                type: "text",
+                nullable: false,
+                defaultValue: ""
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "ConversationType", table: "Conversations");
+        }
+    }
+}
