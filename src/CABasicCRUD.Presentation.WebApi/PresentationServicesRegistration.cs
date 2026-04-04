a2a54495920bc6f4d25036c8bae69f08b0b684a7
Admin <108695191+mgsbb@users.noreply.github.com>
2026-04-05 00:24:59 +0530

    feat(api): send custom error messages for bad json
    Customize error messages according to thrown error.
    Note: There may be other bad inputs that may generate different error messages.

--- src/CABasicCRUD.Presentation.WebApi/PresentationServicesRegistration.cs
+++ src/CABasicCRUD.Presentation.WebApi/PresentationServicesRegistration.cs
@@ -5,6 +5,9 @@ using CABasicCRUD.Presentation.WebApi.Common.Security.Authorization;
 using CABasicCRUD.Presentation.WebApi.Middlewares;
 using CABasicCRUD.Presentation.WebApi.RateLimiter;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CABasicCRUD.Presentation.WebApi;
 
@@ -41,6 +44,62 @@ public static class PresentationServicesRegistration
         services.AddRateLimiter(RateLimitPolicies.Add);
 
         services.AddEndpointsApiExplorer();
+
+        services.ConfigureInvalidModelState();
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureInvalidModelState(this IServiceCollection services)
+    {
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var factory =
+                    context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+
+                var modelState = new ModelStateDictionary();
+
+                var value = context.ModelState;
+
+                foreach (var entry in context.ModelState.Where(x => x.Value?.Errors.Count > 0))
+                {
+                    foreach (var error in entry.Value!.Errors)
+                    {
+                        string errorMessage = "";
+
+                        if (error.ErrorMessage.Contains("The JSON value could not be converted"))
+                        {
+                            errorMessage = "Invalid value.";
+                        }
+                        else if (
+                            error.ErrorMessage.Contains("property name")
+                            || error.ErrorMessage.Contains("invalid start of a value")
+                            || error.ErrorMessage.Contains("invalid after a value")
+                            || error.ErrorMessage.Contains("trailing comma")
+                            || error.ErrorMessage.Contains("invalid end of a number")
+                        )
+                        {
+                            errorMessage = "Invalid JSON format.";
+                        }
+                        else
+                        {
+                            errorMessage = error.ErrorMessage;
+                        }
+
+                        modelState.AddModelError(entry.Key, errorMessage);
+                    }
+                }
+
+                var validation = factory.CreateValidationProblemDetails(
+                    context.HttpContext,
+                    modelState
+                );
+
+                return new BadRequestObjectResult(validation);
+            };
+        });
 
         return services;
     }
