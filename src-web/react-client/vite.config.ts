519a1c360c00e66844155e090a6c2223e890b866
Admin <108695191+mgsbb@users.noreply.github.com>
2026-04-05 17:30:43 +0530

    build(react): add ws to vite proxy

--- src-web/react-client/vite.config.ts
+++ src-web/react-client/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5170',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5170",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
     },
+  },
 });
