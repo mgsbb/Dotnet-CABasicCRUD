abb7c049dd99f473efccfa9b745f9e039910fe3d
Admin <108695191+mgsbb@users.noreply.github.com>
2026-04-14 22:25:33 +0530

    feat(infra): add PostMedia configuration and migrations

---
+++ src/CABasicCRUD.Infrastructure.Persistence.PostgreSql/Migrations/20260414125724_AddPostMediaTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CABasicCRUD.Infrastructure.Persistence.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostMediaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostsMedia",
+                columns: table => new
+                {
+                    PostId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MediaId = table.Column<Guid>(type: "uuid", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostsMedia", x => new { x.PostId, x.MediaId });
+                    table.ForeignKey(
+                        name: "FK_PostsMedia_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade
+                    );
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "PostsMedia");
+        }
+    }
+}
