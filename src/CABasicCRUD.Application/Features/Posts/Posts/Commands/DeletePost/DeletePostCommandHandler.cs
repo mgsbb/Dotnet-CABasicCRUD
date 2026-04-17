6b064c8e02e8488aa9d9dfc07166a4810c8d9735
Admin <108695191+mgsbb@users.noreply.github.com>
2026-04-17 15:57:26 +0530

    fix(app): delete media when deleting post

--- src/CABasicCRUD.Application/Features/Posts/Posts/Commands/DeletePost/DeletePostCommandHandler.cs
+++ src/CABasicCRUD.Application/Features/Posts/Posts/Commands/DeletePost/DeletePostCommandHandler.cs
@@ -2,6 +2,7 @@ using CABasicCRUD.Application.Common.Interfaces;
 using CABasicCRUD.Application.Common.Interfaces.Messaging;
 using CABasicCRUD.Application.Features.Identity.Auth.Common;
 using CABasicCRUD.Domain.Common;
+using CABasicCRUD.Domain.MediaItems;
 using CABasicCRUD.Domain.Posts.Posts;
 
 namespace CABasicCRUD.Application.Features.Posts.Posts.Commands.DeletePost;
@@ -9,12 +10,16 @@ namespace CABasicCRUD.Application.Features.Posts.Posts.Commands.DeletePost;
 internal sealed class DeletePostCommandHandler(
     IPostRepository postRepository,
     IUnitOfWork unitOfWork,
-    ICacheService cacheService
+    ICacheService cacheService,
+    IFileStorage fileStorage,
+    IMediaRepository mediaRepository
 ) : ICommandHandler<DeletePostCommand>
 {
     private readonly IPostRepository _postRepository = postRepository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly ICacheService _cacheService = cacheService;
+    private readonly IMediaRepository _mediaRepository = mediaRepository;
+    private readonly IFileStorage _fileStorage = fileStorage;
 
     public async Task<Result> Handle(DeletePostCommand request, CancellationToken cancellationToken)
     {
@@ -29,6 +34,21 @@ internal sealed class DeletePostCommandHandler(
         if (request.UserId != post.UserId)
         {
             return Result.Failure(AuthErrors.Forbidden);
+        }
+
+        if (post.PostMediaItems.Any())
+        {
+            foreach (PostMedia postMedia in post.PostMediaItems)
+            {
+                Media? media = await _mediaRepository.GetByIdAsync(postMedia.MediaId);
+
+                if (media is not null)
+                {
+                    await _fileStorage.DeleteAsync(media.StorageKey, cancellationToken);
+
+                    await _mediaRepository.DeleteAsync(media);
+                }
+            }
         }
 
         await _postRepository.DeleteAsync(entity: post);
