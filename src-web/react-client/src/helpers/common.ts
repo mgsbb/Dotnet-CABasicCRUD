70deae45b647b43257b1f19182bb81154aacc29a
Admin <108695191+mgsbb@users.noreply.github.com>
2026-04-17 13:51:04 +0530

    refactor(react): refactor post card on post lists page
    Add method to calculate time ago.

--- src-web/react-client/src/helpers/common.ts
+++ src-web/react-client/src/helpers/common.ts
@@ -2,3 +2,33 @@ export function pascalToCamel(input: string): string {
   if (!input) return "";
   return input[0].toLowerCase() + input.slice(1);
 }
+
+export function timeAgo(input: string): string {
+  const date = new Date(input);
+
+  const now = new Date();
+
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  if (seconds < 0) return "just now";
+
+  const intervals: { label: string; seconds: number }[] = [
+    { label: "y", seconds: 31536000 },
+    { label: "mo", seconds: 2592000 },
+    { label: "w", seconds: 604800 },
+    { label: "d", seconds: 86400 },
+    { label: "h", seconds: 3600 },
+    { label: "m", seconds: 60 },
+    { label: "s", seconds: 1 },
+  ];
+
+  for (const interval of intervals) {
+    const count = Math.floor(seconds / interval.seconds);
+
+    if (count >= 1) {
+      return `${count}${interval.label} ago`;
+    }
+  }
+
+  return "just now";
+}
--- src-web/react-client/src/pages/Posts/components/Post.tsx
+++ src-web/react-client/src/pages/Posts/components/Post.tsx
@@ -1,26 +1,86 @@
 import { Link } from "react-router";
 import type { TPost } from "../../../types/posts";
+import { useState } from "react";
+import { timeAgo } from "../../../helpers/common";
 
 export default function Post({ post }: { post: TPost }) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   return (
-    <li key={post.id} className="">
-      <div className="flex flex-col lg:flex-row gap-2 items-start justify-between">
-        <div className="flex flex-col">
-          <div className="flex items-end gap-4">
-            <Link to={`/posts/${post.id}`} className="flex flex-col">
-              <p className="text-gray-700 font-bold">{post.title}</p>
-            </Link>
-            <Link to={`/users/${post.userId}`} className="">
-              <p className="text-sm font-bold text-gray-500 ">
-                - {post.authorName}
-              </p>
-            </Link>
+    <>
+      <div className="rounded-lg border border-gray-300 p-4 space-y-4">
+        {/* author info */}
+        <Link to={`/users/${post.userId}`} className="flex items-center gap-3">
+          <img
+            src={
+              post.authorProfileImageUrl ||
+              "https://api.dicebear.com/7.x/initials/svg?seed=" +
+                post.authorName
+            }
+            className="w-12 h-12 rounded-full object-cover"
+          />
+          <div className="flex flex-col">
+            <span className="font-semibold text-sm">{post.authorName}</span>
+            <span className="text-xs text-gray-500">
+              {timeAgo(post.createdAt)}
+            </span>
           </div>
-          <Link to={`/posts/${post.id}`} className="flex flex-col">
-            <p className="text-gray-600 text-sm">{post.content}</p>
-          </Link>
-        </div>
+        </Link>
+
+        <Link to={`/posts/${post.id}`} className="flex gap-2 flex-col">
+          {/* title */}
+          <h2 className="text-2xl font-semibold ">{post.title}</h2>
+
+          {/* content */}
+          <p className="text-gray-700 text-sm">{post.content}</p>
+        </Link>
+
+        {/* image */}
+        {post.mediaUrls.length > 0 && (
+          <div className="relative group w-full h-87.5 overflow-hidden bg-gray-100 hover:bg-red-100">
+            <img
+              src={post.mediaUrls[currentImageIndex]}
+              className="w-full h-full object-cover"
+            />
+
+            {/* overlay */}
+            <div className="absolute inset-0 bg-black/0 group-hover:bg-black/50 transition duration-300" />
+
+            {/* next/prev button */}
+            {post.mediaUrls.length > 1 && (
+              <>
+                <button
+                  onClick={() => {
+                    setCurrentImageIndex((i) =>
+                      i === 0 ? post.mediaUrls.length - 1 : i - 1,
+                    );
+                  }}
+                  className="absolute left-2 top-1/2 -translate-y-1/2 bg-white/80  
+                  text-black px-2 py-1 rounded-full"
+                >
+                  ‹
+                </button>
+                <button
+                  onClick={() => {
+                    setCurrentImageIndex((i) =>
+                      i === post.mediaUrls.length - 1 ? 0 : i + 1,
+                    );
+                  }}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 bg-white/80  
+                  text-black px-2 py-1 rounded-full"
+                >
+                  ›
+                </button>
+
+                {/* current image indicator */}
+                <div className="absolute bottom-2 right-2 bg-black/60 text-white text-xs px-2 py-1 rounded">
+                  {currentImageIndex + 1}/{post.mediaUrls.length}
+                </div>
+              </>
+            )}
+          </div>
+        )}
       </div>
-    </li>
+    </>
   );
 }
